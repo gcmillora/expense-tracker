afbe8576ad24130825d268c415be4ca103efe4bf
Greg Millora <greg@old.st>
2024-05-29 15:43:12 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ExpenseTable from './components/ExpenseTable'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from './components/ui/button'
+import { Label } from '@radix-ui/react-label'
+import { Input } from './components/ui/input'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'Shopping', amount: 100, dateCreated: new Date() },
+    { id: 2, description: 'Food', amount: 50, dateCreated: new Date() },
+    { id: 3, description: 'Transportation', amount: 25, dateCreated: new Date() },
+  ])
+
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const handleAddExpense = () => {
+    setExpenses([
+      ...expenses,
+      {
+        id: expenses.length + 1,
+        description: description,
+        amount: amount,
+        dateCreated: new Date(),
+      },
+    ])
+  }
+
+  return (
+    <main>
+      <div className='flex flex-col mt-24'>
+        <div className='flex justify-end mb-8'>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline">Add Expense</Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className="grid gap-4">
+                <div className="space-y-2">
+                  <h4 className="font-medium leading-none">Expense</h4>
+                  <p className="text-sm text-muted-foreground">
+                    Add a new expense
+                  </p>
+                </div>
+                <div className="grid gap-2">
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="description" className='text-sm'>Description</Label>
+                    <Input
+                      id="description"
+                      defaultValue="100%"
+                      className="col-span-2 h-8"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="amount" className='text-sm'>Total Amount</Label>
+                    <Input
+                      id="amount"
+                      defaultValue="300px"
+                      className="col-span-2 h-8"
+                      value={amount}
+                      type='number'
+                      onChange={(e) => setAmount(Number(e.target.value))}
+                    />
+                  </div>
+                </div>
+                <Button
+                  type="submit"
+                  variant={"outline"}
+                  onClick={handleAddExpense}
+                  className="w-full h-12 text-black rounded-md text-center"
+                >
+                  Add Expense
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+
+        <ExpenseTable expenses={expenses} />
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseTable.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+
+
+
+
+const ExpenseTable = ({ expenses }) => {
+    const total: number = expenses.reduce((acc: number, expense: any) => acc + expense.amount, 0)
+    return (
+        <Table>
+            <TableCaption>List of Expenses</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[100px]">ID</TableHead>
+                    <TableHead>Description</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {
+                    expenses.map((expense) => (
+                        <TableRow key={expense.id}>
+                            <TableCell>{expense.id}</TableCell>
+                            <TableCell>{expense.description}</TableCell>
+                            <TableCell>{expense.dateCreated.toDateString()}</TableCell>
+                            <TableCell className="text-right">{expense.amount}</TableCell>
+                        </TableRow>
+                    ))
+                }
+            </TableBody>
+            <TableFooter>
+                <TableRow>
+                    <TableCell colSpan={3} className='text-left'>Total</TableCell>
+                    <TableCell className="text-right">{`Php ${total.toString()}`}</TableCell>
+                </TableRow>
+            </TableFooter>
+        </Table>
+    )
+}
+
+export default ExpenseTable
